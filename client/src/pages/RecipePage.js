e9e8d85cf155968be75ab3f32fc0b941202ce24c
almoral323 <almoral323@gmail.com>
2023-03-15 19:06:37 -0400

    latest changes

---
+++ client/src/pages/RecipePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RecipeDisplay from "../components/RecipeDisplay";
+
+
+const RecipePage= () => {
+    return <div>
+        <RecipeDisplay></RecipeDisplay>
+    </div>
+}
+
+export default RecipePage;
